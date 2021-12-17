1d0702ef05b42d8d0858fd07c655ec282b3695ce
MarkTsengTW <96064882+MarkTsengTW@users.noreply.github.com>
2021-12-17 22:03:53 +0800

    Update code.py

--- code.py
+++ code.py
@@ -30,11 +30,15 @@ network = Network(status_neopixel=board.NEOPIXEL, debug=False)
 # --- Drawing setup ---
 group = displayio.Group()  # Create a Group
 bitmap = displayio.Bitmap(64, 32, 2)  # Create a bitmap object,width, height, bit depth
-color = displayio.Palette(4)  # Create a color palette
-color[0] = 0x000000  # black background
+color = displayio.Palette(8)  # Create a color palette
+color[0] = 0x000000  # black
 color[1] = 0xFF0000  # red
-color[2] = 0xCC4000  # amber
-color[3] = 0x85FF00  # greenish
+color[2] = 0xCC4000  # orange
+color[3] = 0x85FF00  # yellow
+color[4] = 0x00FF00  # green
+color[5] = 0x0000FF  # blue
+color[6] = 0xFF00FF  # purple
+color[7] = 0xFF40AA  # pink
 
 # Create a TileGrid using the Bitmap and Palette
 tile_grid = displayio.TileGrid(bitmap, pixel_shader=color)
@@ -49,16 +53,12 @@ else:
 hours_label = Label(font)
 colon_label = Label(font)
 minutes_label = Label(font)
-minutes_label2 = Label(font)
+minutes_second_digit_label = Label(font)
 
 def update_time(*, hours=None, minutes=None, show_colon=False):
     now = time.localtime()  # Get the time values we need
     if hours is None:
         hours = now[3]
-    if hours >= 18 or hours < 6:  # evening hours to morning
-        hours_label.color = color[1]
-    else:
-        hours_label.color = color[3]  # daylight hours
     if hours > 12:  # Handle times later than 12:59
         hours -= 12
     elif not hours:  # Handle times between 0:00 and 0:59
@@ -69,11 +69,17 @@ def update_time(*, hours=None, minutes=None, show_colon=False):
 
     colon = ":"
     if BLINK:
-        colon_label.color = color[2] if show_colon or now[5] % 2 else color[0]
+        colon_label.color = color[3] if show_colon or now[5] % 2 else color[0]
     else:
         colon_label.color = color[0]
 
-   
+    if DEBUG: 
+        print("Hours is {} and minutes is {}".format(hours, minutes))
+
+    hours_label.color = color[1]
+    minutes_label.color = color[4]
+    minutes_second_digit_label.color = color[5]
+
     #We need a label for each colour we're going to display. Starting with colon.
     colon_label.text = "{colon}".format(
         colon=colon
@@ -84,8 +90,8 @@ def update_time(*, hours=None, minutes=None, show_colon=False):
     colon_label.y = display.height // 2
     if DEBUG:
         print("Label bounding box: {},{},{},{}".format(cbbx, cbby, cbbwidth, cbbh))
-        print("Label x: {} y: {}".format(colon_label.x, colon_label.y))  
-    
+        print("Label x: {} y: {}".format(colon_label.x, colon_label.y))
+
     #Hours label
     hours_label.text = "{hours}".format(
         hours=hours)
@@ -96,10 +102,10 @@ def update_time(*, hours=None, minutes=None, show_colon=False):
     if DEBUG:
         print("Label bounding box: {},{},{},{}".format(hbbx, hbby, hbbwidth, hbbh))
         print("Label x: {} y: {}".format(hours_label.x, hours_label.y))
- 
-  #Minutes label
-    minutes_label.text = "{minutes:02d}".format(
-        minutes=minutes
+
+    #First minutes label.
+    minutes_label.text = "{minutes}".format(
+    minutes = (minutes // 10)
     )
     mbbx, mbby, mbbwidth, mbbh = minutes_label.bounding_box
     # Center the label
@@ -108,25 +114,26 @@ def update_time(*, hours=None, minutes=None, show_colon=False):
     if DEBUG:
         print("Label bounding box: {},{},{},{}".format(mbbx, mbby, mbbwidth, mbbh))
         print("Label x: {} y: {}".format(minutes_label.x, minutes_label.y))
- 
+
     #Second minutes label
-    minutes_label2.text = "{minutes:02d}".format(
-        minutes=minutes
+    minutes_second_digit_label.text = "{minutes}".format(
+        minutes = (minutes % 10)
     )
-    m2bbx, m2bby, m2bbwidth, m2bbh = minutes_label2.bounding_box
+    m2bbx, m2bby, m2bbwidth, m2bbh = minutes_second_digit_label.bounding_box
     # Center the label
-    minutes_label2.x = round(display.width / 2 + cbbwidth + mbbwidth)
-    minutes_label2.y = display.height // 2
+    minutes_second_digit_label.x = round(display.width / 2 + cbbwidth + mbbwidth)
+    minutes_second_digit_label.y = display.height // 2
     if DEBUG:
         print("Label bounding box: {},{},{},{}".format(m2bbx, m2bby, m2bbwidth, m2bbh))
-        print("Label x: {} y: {}".format(minutes_label2.x, minutes_label2.y))
+        print("Label x: {} y: {}".format(minutes_second_digit_label.x, minutes_second_digit_label.y))
 
 
 last_check = None
-update_time(show_colon=True)  # Display whatever time is on the board
-group.append(hours_label)  # add the clock label to the group
-group.append(colon_label)  # add the clock label to the group
-group.append(minutes_label)  # add the clock label to the group
+update_time(show_colon=True)  
+group.append(hours_label)  
+group.append(colon_label)  
+group.append(minutes_label)  
+group.append(minutes_second_digit_label)  
 
 while True:
     if last_check is None or time.monotonic() > last_check + 3600:
